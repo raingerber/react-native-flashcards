d869a8efee755d7eea31cdcc1b654f4630e0d690
Matt Armstrong <raingerber2@gmail.com>
2017-11-05 18:15:13 -0500

    refactor: add separate export object in storage.js

--- storage.js
+++ storage.js
@@ -4,19 +4,19 @@ const KEY = 'flashcards-storage-key'
 
 const newDeck = (title, questions = []) => ({ title, questions })
 
-export const getDecks = () => {
+const getDecks = () => {
   return AsyncStorage.getItem(KEY).then((decks) => JSON.parse(decks) || {})
 }
 
-export const getDeck = (title) => {
+const getDeck = (title) => {
   return getDecks().then((decks) => decks[title])
 }
 
-export const saveDeckTitle = (title) => {
+const saveDeckTitle = (title) => {
   return saveDeck(title, newDeck(title))
 }
 
-export const addCardToDeck = (title, card) => {
+const addCardToDeck = (title, card) => {
   return getDeck(title).then((deck) => {
     deck = deck || newDeck(title)
     deck.questions.push(card)
@@ -24,6 +24,15 @@ export const addCardToDeck = (title, card) => {
   })
 }
 
-export const saveDeck = (title, deck) => {
+const saveDeck = (title, deck) => {
   return AsyncStorage.mergeItem(KEY, JSON.stringify({ [title]: deck }))
+    .then(() => getDecks())
+}
+
+export {
+  getDecks,
+  getDeck,
+  saveDeckTitle,
+  addCardToDeck,
+  saveDeck
 }
