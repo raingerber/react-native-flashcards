8971e0cd9a684057af0acf6e1161d32c94348a5e
Matthew Armstrong <matthewr.armstrong@viacom.com>
2017-11-06 13:30:59 -0500

    feat: setup local notifications in App componentDidMount method

--- App.js
+++ App.js
@@ -3,6 +3,8 @@ import {createStore, applyMiddleware} from 'redux'
 import thunk from 'redux-thunk'
 import {Provider} from 'react-redux'
 
+import {setLocalNotification} from './notifications'
+
 import reducer from './reducers/index'
 
 import MainView from './components/MainView'
@@ -10,6 +12,10 @@ import MainView from './components/MainView'
 const store = createStore(reducer, applyMiddleware(thunk))
 
 export default class App extends React.Component {
+  componentDidMount () {
+    setLocalNotification()
+  }
+
   render () {
     return (
       <Provider store={store}>
