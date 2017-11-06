5febb5c70804086a8effa60c3d5b3fbec27ab668
Matthew Armstrong <matthewr.armstrong@viacom.com>
2017-11-06 13:16:58 -0500

    refactor: remove some unused redux actions and action types

--- actions/index.js
+++ actions/index.js
@@ -1,9 +1,7 @@
 import {
-  GET_DECK,
   GET_DECKS,
   SAVE_DECK_TITLE,
-  ADD_CARD_TO_DECK,
-  SET_CURRENT_DECK
+  ADD_CARD_TO_DECK
 } from './types'
 
 import * as Storage from '../storage'
@@ -11,12 +9,6 @@ import * as Storage from '../storage'
 const getDecks = () => (dispatch) => {
   return Storage.getDecks()
     .then((decks) => dispatch({type: GET_DECKS, decks}))
-}
-
-// TODO always call getDecks before calling getDeck?
-const getDeck = (title) => (dispatch) => {
-  return Storage.getDeck(title)
-    .then((deck) => dispatch({type: GET_DECK, deck}))
 }
 
 const saveDeckTitle = (title) => (dispatch) => {
@@ -29,14 +21,8 @@ const addCardToDeck = (title, card) => (dispatch) => {
     .then((decks) => dispatch({type: ADD_CARD_TO_DECK, decks}))
 }
 
-const setCurrentDeck = (deck) => (dispatch) => {
-  return dispatch({type: SET_CURRENT_DECK, deck})
-}
-
 export {
   getDecks,
-  getDeck,
   saveDeckTitle,
-  addCardToDeck,
-  setCurrentDeck
+  addCardToDeck
 }
